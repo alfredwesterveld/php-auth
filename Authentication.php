66761916c0654bcafc1955d82fdb226c7c22aed9
Alfred Westerveld <alfredwesterveld@gmail.com>
2011-01-28 23:37:25 +0100

    replaced for PDO's named parameters mechanism

--- Authentication.php
+++ Authentication.php
@@ -17,9 +17,10 @@ class Authentication {
     }
     
     public function createTables() {
-        $this->db->exec("CREATE TABLE IF NOT EXISTS users " .
-        "(id INTEGER PRIMARY KEY , username TEXT NOT NULL UNIQUE, " .
-        "email TEXT NOT NULL, hash TEXT NOT NULL, active BOOLEAN)");
+        $this->db->exec(
+            "CREATE TABLE IF NOT EXISTS users " .
+            "(id INTEGER PRIMARY KEY , username TEXT NOT NULL UNIQUE, " .
+            "email TEXT NOT NULL, hash TEXT NOT NULL, active BOOLEAN)");
     }
 
     /**
@@ -28,39 +29,53 @@ class Authentication {
      * @return <type>
      */
     public function checkEmailIsUnique($email) {
-        $placeholders = array($email);
-        $stmt = $this->db->prepare("SELECT email FROM users WHERE email = ?");
-        $stmt->execute($placeholders);
+        $stmt = $this->db->prepare(
+            "SELECT email FROM users WHERE email = :email");
+        $stmt->execute(array(
+            ":email" => $email
+        ));
         $row = $stmt->fetch();
         return $row['email'] != $email;
     }
 
     public function checkUsernameIsUnique($username) {
-        $placeholders = array($username);
-        $stmt = $this->db->prepare("SELECT username FROM users WHERE username = ?");
-        $stmt->execute($placeholders);
+        $stmt = $this->db->prepare(
+            "SELECT username FROM users WHERE username = :username");
+        $stmt->execute(array(
+            ":username" => $username
+        ));
         $row = $stmt->fetch();
         return $row['username'] != $username;
     }
 
     public function create($username, $email, $password) {
         $hash = $this->hasher->HashPassword($password);
-        if (strlen($hash) < 20)
+        if (strlen($hash) < 20) {
 	        throw new Exception('Failed to hash new password');
+        }
 
-        if (!$this->checkEmailIsUnique($email))
+        if (!$this->checkEmailIsUnique($email)) {
                 return false;
+        }
 
-        $placeholders = array($username, $email, $hash);
-        $stmt = $this->db->prepare("INSERT OR IGNORE INTO users (username, email, hash) VALUES (?, ?, ?)");
-        $stmt->execute($placeholders);
+        $stmt = $this->db->prepare(
+            "INSERT OR IGNORE INTO users (username, email, hash) " .
+            "VALUES (:username, :email, :hash)");
+        $stmt->execute(array(
+            ":username" => $username,
+            ":email" => $email,
+            ":hash" => $hash
+        ));
         return $stmt->rowCount() == 1;
     }
     
     public function login($credentials, $password) {
-        $placeholders = array($credentials, $credentials);
-        $stmt = $this->db->prepare("SELECT hash FROM users WHERE username = ? OR email = ?");
-        $stmt->execute($placeholders);
+        $stmt = $this->db->prepare(
+            "SELECT hash FROM users WHERE username = :credentials " .
+            "OR email = :credentials");
+        $stmt->execute(array(
+            ":credentials" => $credentials
+        ));
         $row = $stmt->fetch();
         if ($this->hasher->CheckPassword($password, $row['hash'])) {
             return true;
@@ -70,28 +85,37 @@ class Authentication {
     }
     
     public function isActivated($username) {
-        $placeholders = array($username);
-        $stmt = $this->db->prepare("SELECT active FROM users WHERE username = ?");
-        $stmt->execute($placeholders);
+        $stmt = $this->db->prepare(
+            "SELECT active FROM users WHERE username = :username");
+        $stmt->execute(array(
+            ":username" => $username
+        ));
         $row = $stmt->fetch();
         return $row['active'] == true;
     }
     
     public function createEmailVerificationSecret($email) {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false)
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
 	        throw new EmailException();
+        }
 
         $hash = $this->hasher->HashPassword($email);
-        if (strlen($hash) < 20)
+        if (strlen($hash) < 20) {
 	        throw new Exception('Failed to hash new password');
+        }
         return $hash;
     }
     
     public function verifyEmail($credentials, $secret) {
         if ($this->hasher->CheckPassword($credentials, $secret)) {
             $placeholders = array(true, $credentials, $credentials);
-            $stmt = $this->db->prepare("UPDATE users SET active = ? WHERE username = ? OR email = ?");                  
-            $stmt->execute($placeholders);
+            $stmt = $this->db->prepare(
+                "UPDATE users SET active = :active WHERE " .
+                "username = :credentials OR email = :credentials");
+            $stmt->execute(array(
+                ":active" => true,
+                ":credentials" => $credentials
+            ));
             return true;
 	    } else {
 	        return false;
